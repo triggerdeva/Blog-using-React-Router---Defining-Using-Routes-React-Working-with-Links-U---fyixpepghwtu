46692576dd9ac27bf0391c45928e28578fbe8dda
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-03-24 18:18:07 +0530

    Update PostDetail.js

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -2,5 +2,10 @@ import React from 'react';
 
 export default function PostDetail({ match }) {
   // Complete the function
+  // console.log(match)
+  return(
+    <h1>Post Detail: {match.params.id}</h1>
+  )
+
 }
-  
+ 
